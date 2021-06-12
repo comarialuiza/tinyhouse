85df9a7a0ca20a207aab481ad3e94e2ca6d074d7
Maria Luiza <co.maria.luiza@gmail.com>
2021-06-11 22:56:00 -0300

    connects server with database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,19 @@
+require('dotenv').config();
+
 import { ApolloServer } from 'apollo-server-express';
-import express from 'express';
+import express, { Application } from 'express';
 import { resolvers } from './graphql/resolvers';
 import { typeDefs } from './graphql/typeDefs';
+import { connectDatabase } from './database';
 
-const app = express();
-const port = 9000;
+const port = process.env.PORT;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/api' });
+const mount = async (app: Application) => {
+    const db = await connectDatabase();
+    const server = new ApolloServer({ typeDefs, resolvers, context: () => ({ db }) });
+    server.applyMiddleware({ app, path: '/api' });
+    
+    app.listen(port);
+}
 
-app.listen(port);
-console.log('i am running!');
+mount(express());
